373397cb1e40fea101a412521349cc9908eaa4fa
kelvin defrNBAH <kevinocholah@gmail.com>
2024-04-13 21:48:51 +0300

    initiating sql successively

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
-"""
-List all states from the database hbtn_0e_0_usa
-"""
+#!/usr/bin/env python3
+"""" lists all the states in the db created """
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="local", port=3306, user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
