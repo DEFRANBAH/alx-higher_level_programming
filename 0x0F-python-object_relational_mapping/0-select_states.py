c005f91034645edeb0bc68026986084c6b8343e3
kelvin defrNBAH <kevinocholah@gmail.com>
2024-04-13 22:02:23 +0300

    initiating sql successively

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,14 +7,14 @@ import sys      # import the sys module
 
 if __name__ == "__main__":
     """ a script that lists all states from the database hbtn_0e_0_usa """
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])    # connect to the database
-    cur = db.cursor()   # instantiate a cursor obj
+    cur = conn.cursor()   # instantiate a cursor obj
     cur.execute("SELECT * FROM states ORDER BY id ASC")    # execute the query
     rows = cur.fetchall()    # fetch all the rows
     for row in rows:    # loop through the rows
         print(row)    # print the row       
     cur.close()
-    db.close()    # close the connection
+    conn.close()    # close the connection
 
 
