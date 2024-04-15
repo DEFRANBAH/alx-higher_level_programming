790739b6ccf06e804fa390124f1791c6aaeb485f
kelvin defrNBAH <kevinocholah@gmail.com>
2024-04-16 00:01:06 +0300

    alchemy init

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
-This module defines a class called State that inherits from Base.
-these are instances of declarative_base() class.
+Contains State class and Base, an instance of declarative_base()
 """
-
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 class State(Base):
     """
-    State class that inherits from Base with id and name attributes.
+    Class with id and name attributes of each state
     """
-
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
