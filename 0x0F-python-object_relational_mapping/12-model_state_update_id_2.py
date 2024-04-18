2649b5dfaa03926e44bcf5172c611f087583ee95
kelvin defrNBAH <kevinocholah@gmail.com>
2024-04-18 03:07:42 +0300

    advance : Relationship member

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+""" updates the State instance id with a different instance name attribute passed as argument from the database
 """
 import sys
 from model_state import Base, State
